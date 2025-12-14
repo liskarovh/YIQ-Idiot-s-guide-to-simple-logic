656f1fd295dad6b34e943333976949f38665034d
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-12-14 03:54:22 +0100

    fix: correct API_BASE URL construction in MinesweeperApiClient
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- src/Frontend/src/minesweeper/models/MinesweeperApiClient.jsx
+++ src/Frontend/src/minesweeper/models/MinesweeperApiClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-console.log("API Base URL:", process.env.REACT_APP_API_URL);
-export const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api/minesweeper";
+const APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_BASE = `${APP_API_URL}/api/minesweeper`;
 
 const axiosOptions = {
     baseURL: API_BASE,
